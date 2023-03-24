e394006b99d09d45e7223290a520521c61dd6d96
Beata Ambroziak <beataxambroziak@gmail.com>
2023-03-24 12:40:30 +0100

    js update - function added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
-console.log("Hello World");
-
-let supriseText = document.querySelector(".js-supriseText");
-
-let suprise = document.querySelector(".js-suprise");
-
-suprise.addEventListener("click", () => {
-    supriseText.innerText = supriseText.innerText === ":) :) :)"
-        ? "Zapraszamy na Maltę!"
-        : ":) :) :)";
-});
-
-let button = document.querySelector(".js-button")
-
-button.addEventListener("click", () => {
-    if (button.innerText === "Spodobała Ci się Malta?") {
-        button.innerText = "Zarezerwuj lot już teraz!";
-    } else {
-        button.innerText = "Spodobała Ci się Malta?";
-    }
-})
-
-let changeStyle = document.querySelector(".js-changeStyle")
-let box = document.querySelector(".js-wrapper");
-let themeName = document.querySelector(".js-themeName")
-
-changeStyle.addEventListener("click", () => {
-    box.classList.toggle("navigation__changeStyle--dark");
-
-    if (box.classList.contains("navigation__changeStyle--dark")) {
-        themeName.innerText = "jasny";
-    } else {
-        themeName.innerText = "ciemny";
+{
+    const welcome = () => {
+        console.log("Hello World!!!");
     }
 
-})
+    const toggleBackground = () => {
+        const box = document.querySelector(".js-wrapper");
+        const themeName = document.querySelector(".js-themeName")
+        box.classList.toggle("navigation__changeStyle--dark");
+        if (box.classList.contains("navigation__changeStyle--dark")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = "ciemny";
+        }
+    };
+
+    const toggleText = () => {
+        const supriseText = document.querySelector(".js-supriseText");
+        supriseText.innerText = supriseText.innerText === ":) :) :)"
+            ? "Zapraszamy na Maltę!"
+            : ":) :) :)";
+    };
+
+    const buttonToggleText = () => {
+        const button = document.querySelector(".js-button");
+        if (button.innerText === "Spodobała Ci się Malta?") {
+            button.innerText = "Zarezerwuj lot już teraz!";
+        } else {
+            button.innerText = "Spodobała Ci się Malta?";
+        }
+    };
+
+    const init = () => {
+        const changeStyle = document.querySelector(".js-changeStyle");
+        changeStyle.addEventListener("click", toggleBackground);
+        const suprise = document.querySelector(".js-suprise");
+        suprise.addEventListener("click", toggleText);
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", buttonToggleText);
+        welcome();
+    };
+
+    init();
+}
